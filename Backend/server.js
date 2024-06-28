0c13a9c082bb467af539503ebd8cfd574b6cc258
dushyant264 <ddushyant89@gmail.com>
2024-06-29 02:22:55 +0530

    ignore changes

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');  // environment variable
 // required modules
 
 const express = require('express');
+const cors = require('cors');
 const colors = require('colors');
 const { connectDB } = require('./config/db');
 
