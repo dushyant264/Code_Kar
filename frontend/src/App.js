05041ac60d833d53233f3b6a639954468b57adf9
dushyant264 <ddushyant89@gmail.com>
2024-07-06 04:37:17 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect, useState, createContext } from 'react'
 import Cookies from 'js-cookie'
+// import components
+import Navbar from './components/NavBar'
 
 const UserContext= createContext()
 
@@ -34,10 +36,11 @@ function App() {
   return (
    <UserContext.Provider value={{ user, token, setToken, isLoggedIn, setIsLoggedIn}}>
     <div className='App'>
-
+      <Navbar/>
     </div>
    </UserContext.Provider>
   );
 }
 
 export default App;
+export { UserContext }
