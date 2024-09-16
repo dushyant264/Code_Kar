659c4ada59a9f04ad922ddf21a25c666b25c7323
dushyant264 <ddushyant89@gmail.com>
2024-09-16 16:46:26 +0530

    error display

--- Backend/Controllers/UserController/checkProblem.js
+++ Backend/Controllers/UserController/checkProblem.js
@@ -80,7 +80,7 @@ const checkProblemController = async (req, res) => {
         
     } catch (error) {
         console.log(error)
-        res.status(500).json({message: 'Internal Server Error'})
+        res.status(500).json(error)
     }
 }
 
